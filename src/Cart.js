a61f0c53b63ef9ff141d97ab2ff44ef01fab0dc9
Raymond Martinez <rcmartinez1854@gmail.com>
2019-11-05 16:42:08 -0800

    added appropriate code from original App.js code

---
+++ src/Cart.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import CartOption from './CartOption';
+import CartTotal from './CartTotal';
+
+class Cart extends React.Component {
+    render() {
+        const contents = Object.keys(this.props.selected).map(key => (
+            <CartOption 
+                key={key}
+                label={key}
+                name={this.props.selected[key].name}
+                cost={this.props.selected[key].cost}
+            />
+        ));
+
+        return (
+            <section className="cart">
+                <h3>here is my h3</h3>
+                {contents}
+                <CartTotal selected={this.props.selected} />
+            </section>
+        )
+    }
+}
+
+export default Cart;
